ca4aa03cdd7b8cc2d241524a424662e1f0eab367
sigmdel <michel@sigmdel.ca>
2026-01-29 02:04:44 -0400

    New system information sketch

---
+++ 00_sys_info/sys_info/sys_info.ino
@@ -0,0 +1,9 @@
+/*
+ * ChipInfo
+ * Display some information about a ESP32 SoC
+ *
+ *
+ * Copyright (C) 2021-2026 Michel Deslierres
+ */
+/* SPDX-License-Identifier: 0BSD */
+/* SPDX-URL: https://spdx.org/licenses/0BSD.html */
