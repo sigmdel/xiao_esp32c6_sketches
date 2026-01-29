ec14723da24f6e912add0908f1bfe27c24893988
sigmdel <michel@sigmdel.ca>
2026-01-29 02:07:07 -0400

    Corrections and catching some invalid or missing macros at compile time

--- 01_pin_names/pin_names/main.cpp
+++ 01_pin_names/pin_names/main.cpp
@@ -1,16 +1,19 @@
-// Main module of pin_names.ino
-// Copyright: see notice in pin_names.ino
-
-// The XIAO ESP32C6 pin definitions are found in 
-// https://github.com/espressif/arduino-esp32/blob/master/variants/XIAO_ESP32C6/pins_arduino.h
-// The BOOT_PIN definition for all ESP32s found in
-// https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal.h
+/*
+ *  See pin_names.ino for license and attribution.
+ */
 
 #include <Arduino.h>
 
-void iopins(void) {
-  Serial.println("\n\nXIAO ESP32C6 I/O Pin Names and Numbers (defined in pins_arduino.h)");
+#if !defined(ARDUINO_XIAO_ESP32C6)
+  #error This program is meant to run on the XIAO ESP32C6 only
+#endif
 
+#if (ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3, 3, 6))    
+  #warning Version 3.3.6 or newer of ESP32 Arduino core version is available
+#endif
+
+void iopins(void) {
+  Serial.println("\n\nXIAO ESP32C6 I/O pins and macros");
   Serial.println("\nThe symbolic name and corresponding I/O number of the 11 digital pins");
   Serial.printf(" D0 = %2d\n", D0);
   Serial.printf(" D1 = %2d\n", D1);
@@ -30,64 +33,90 @@ void iopins(void) {
   Serial.printf(" A2 = %d\n", A2);
   Serial.println("Note: Ax = Dx for x = 0, 1 and 2");
   
-  Serial.println("\nThe symbolic name and corresponding I/O number of the 7 serial pins");
+  Serial.println("\nThe symbolic name and corresponding I/O number of the 8 serial pins");
   Serial.printf("  TX = %2d [UART] (=D6)\n", TX);
   Serial.printf("  RX = %2d [UART] (=D7)\n", RX);
-
   Serial.printf(" SDA = %2d [I2C]  (=D4)\n", SDA);
   Serial.printf(" SCL = %2d [I2C]  (=D5)\n", SCL);
-
   Serial.printf("  SS = %2d [SPI]  (=D3)\n", SS);
   Serial.printf("MOSI = %2d [SPI]  (=D10)\n", MOSI);
   Serial.printf("MISO = %2d [SPI]  (=D9)\n", MISO);
   Serial.printf(" SCK = %2d [SPI]  (=D8)\n", SCK);
-  
+ 
   Serial.println("\nOnboard yellow LED");
   Serial.printf("LED_BUILTIN = %d\n", LED_BUILTIN);
   Serial.printf("BUILTIN_LED = %d // backward compatibility\n", BUILTIN_LED);
 
-  Serial.println("\nAntenna");
-  if (ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 4)) {
-    Serial.printf("    WIFI_ENABLE = %d  (RF switch power enable I/O)\n", WIFI_ENABLE);
-    Serial.printf("WIFI_ANT_CONFIG = %d (RF switch select control I/O)\n", WIFI_ANT_CONFIG);
-  } else {
-    Serial.printf("RF switch I/O not defined in ESP32 %s\n", ESP_ARDUINO_VERSION_STR);
-    Serial.println("  RF switch power enable I/O = 3   (= WIFI_ENABLE     in ESP32 core 3.0.4 or later)");
-    Serial.println("RF switch select control I/O = 14  (= WIFI_ANT_CONFIG in ESP32 core 3.0.4 or later");
-  }
-
   #ifdef BOOT_PIN
-  Serial.println("\nBoot Button (defined in esp32-hal.h)");
+  Serial.println("\nOnboard boot button");
   Serial.printf("BOOT_PIN = %2d\n", BOOT_PIN);
-  #else
-  Serial.println("BOOT_PIN not defined")
   #endif
 
-  Serial.println("\nOther macros"); 
-  Serial.printf("USB_VID          = 0x%04x\n", USB_VID);
-  Serial.printf("USB_PID          = 0x%04x\n", USB_PID);
-  Serial.printf("USB_MANUFACTURER = \"%s\"\n", USB_MANUFACTURER);
-  Serial.printf("USB_PROUCT       = \"%s\"\n", USB_PRODUCT);
-  Serial.printf("USB_SERIAL       = \"%s\"\n", USB_SERIAL);
+  Serial.println("\nAntenna pins");
+  Serial.printf("    WIFI_ENABLE = %d (RF switch power enable I/O)\n", WIFI_ENABLE);
+  Serial.printf("WIFI_ANT_CONFIG = %d (RF switch select control I/O)\n", WIFI_ANT_CONFIG);
   
-  Serial.println("\nBoard macro");
+  Serial.println("\nOther macros"); 
+  Serial.printf("USB_VID          defined = 0x%04x\n", USB_VID);
+  Serial.printf("USB_PID          defined = 0x%04x\n", USB_PID);
+  Serial.printf("USB_MANUFACTURER defined = \"%s\"\n", USB_MANUFACTURER);
+  Serial.printf("USB_PROUCT       defined = \"%s\"\n", USB_PRODUCT);
+  Serial.printf("USB_SERIAL       defined = \"%s\"\n", USB_SERIAL);
+  #ifdef LED_BUILTIN
+  Serial.printf("LED_BUILTIN      defined = %d\n", LED_BUILTIN);
+  #else
+    #error LED_BUILTIN should be defined
+  #endif
+  #ifdef BUILTIN_LED
+  Serial.printf("BUILTIN_LED      defined = %d //deprecated, use LED_BUILTIN\n", BUILTIN_LED);
+  #else
+    #error BUILTIN_LED should be defined
+  #endif
+  #ifdef BOOT_PIN
+  Serial.printf("BOOT_PIN         defined = %d\n", BOOT_PIN);
+  #else
+    #error BOOT_PIN should be defined in esp32-hal.h
+  #endif
+  
+  Serial.println("\nPackage macro");
+  #ifdef ESP32
+  Serial.println("ESP32 defined");
+  #else
+    #error ESP32 should be defined
+  #endif
+
+  Serial.println("\nBoard macros");
   #ifdef ARDUINO_XIAO_ESP32C6
   Serial.println("ARDUINO_XIAO_ESP32C6 defined");
   #else
-  Serial.println("ARDUINO_XIAO_ESP32C6 not defined");
-  #ifdef ARDUINO_XIAO_ESP32C3
-  Serial.println("ARDUINO_XIAO_ESP32C3 incorrectly defined"); // prior to version 3.0.2 esp32
+    #error ARDUINO_XIAO_ESP32C6 should be defined
   #endif
-  #endif
+
+  Serial.println("\nNative USB support");
+  #ifdef ARDUINO_USB_CDC_ON_BOOT 
+  Serial.printf("ARDUINO_USB_CDC_ON_BOOT defined = %d\n", ARDUINO_USB_CDC_ON_BOOT);
+  #else
+    #error ARDUINO_USB_CDC_ON_BOOT=1 should be defined
+  #endif                                                       
 }
 
 void setup() {
+  // Delay to allow for the initialization of the native USB peripheral
+  // and some time for the IDE to reconnect 
+  #if defined(PLATFORMIO)
+    #define SERIAL_BEGIN_DELAY 5000    // 5 seconds
+  #else
+    #define SERIAL_BEGIN_DELAY 2000    // 2 second
+  #endif 
+
   Serial.begin();
-  delay(1000); // 1 second delay should be sufficient for USB-CDC
-  Serial.println("Setup completed");
+  delay(SERIAL_BEGIN_DELAY);
+  Serial.println("\n\nProject: pin_names.ino");
+  Serial.println("Purpose: Print I/O Pin Names, GPIO Numbers, etc.");
+  Serial.println("  Board: XIAO ESP32C6");
 }
 
 void loop() {
   iopins();
-  delay(10000); // wait 10 seconds
+  delay(15000); // wait 15 seconds
 }
--- 01_pin_names/pin_names/pin_names.ino
+++ 01_pin_names/pin_names/pin_names.ino
@@ -1,21 +1,24 @@
 /*
  * pin_names.ino
- * Lists XIAO ESP32C6 I/O pin numbers and their Arduino names
+ * Lists XIAO ESP32C6 I/O pin numbers, their Arduino names and some macros
  *
  * This is a stub to satisfy the Arduino IDE, the source code is in
  * the file main.cpp in the same directory.
+ *
+ * The XIAO ESP32C6 pin definitions are found in 
+ *    https://github.com/espressif/arduino-esp32/blob/master/variants/XIAO_ESP32C6/pins_arduino.h
+ * The BOOT_PIN definition for all ESP32s found in
+ *    https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal.h
  *
  * This sketch will compile in the Arduino IDE
  *
  *  1- Add https://espressif.github.io/arduino-esp32/package_esp32_index.json
  *     in the Additional Boards Manager URLS in the Preferences window.
- *  2- Install platform esp32 by Espressif version 3.0.2 or newer with the Boards Manager
+ *  2- Install platform esp32 by Espressif version 3.3.6 or newer with the Boards Manager
  *  3- Select the XIAO_ESP32C6 board
  *
- * Michel Deslierres
- * June 25, 2024
  *
- * Copyright 2024, Michel Deslierres. No rights reserved, this code is in the public domain.
+ * Copyright 2024,2026 Michel Deslierres. No rights reserved, this code is in the public domain.
  * In those jurisdictions where this may be a problem, the BSD Zero Clause License applies.
  * <https://spdx.org/licenses/0BSD.html> */
  // SPDX-License-Identifier: 0BSD
