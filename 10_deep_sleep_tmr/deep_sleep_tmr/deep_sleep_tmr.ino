9cc0d3b9fd7b9500053d43d3afaca619062af1c7
sigmdel <michel@sigmdel.ca>
2026-02-15 15:39:06 -0400

    Updad to ESP32 3.3.6

--- 10_deep_sleep_tmr/deep_sleep_tmr/deep_sleep_tmr.ino
+++ 10_deep_sleep_tmr/deep_sleep_tmr/deep_sleep_tmr.ino
@@ -1,6 +1,6 @@
 /*
  * deep_sleep_tmr.ino
- * Puts XIAO ESP32C6 in deep sleep mode, with timed wakeups.
+ * Wake XIAO ESP32C5 from deep sleep mode with timer.
  *
  * This is a stub to satisfy the Arduino IDE, the source code is in
  * the file main.cpp in the same directory.
@@ -9,7 +9,7 @@
  *
  *  1- Add https://espressif.github.io/arduino-esp32/package_esp32_index.json
  *     in the Additional Boards Manager URLS in the Preferences window.
- *  2- Install platform esp32 by Espressif version 3.0.2 or newer with the Boards Manager
+ *  2- Install platform esp32 by Espressif version 3.3.6 or newer with the Boards Manager
  *  3- Select the XIAO_ESP32C6 board
  * 
  * References: 
@@ -22,9 +22,9 @@
  *   by PJ Glasso. 
  *
  * Michel Deslierres
- * July 9, 2024
+ * January 20, 2026
  *
- * Copyright 2024, Michel Deslierres. No rights reserved, this code is in the public domain.
+ * Copyright 2024-2026, Michel Deslierres. No rights reserved, this code is in the public domain.
  * In those jurisdictions where this may be a problem, the BSD Zero Clause License applies.
  * <https://spdx.org/licenses/0BSD.html> */
  // SPDX-License-Identifier: 0BSD                
--- 10_deep_sleep_tmr/deep_sleep_tmr/main.cpp
+++ 10_deep_sleep_tmr/deep_sleep_tmr/main.cpp
@@ -1,39 +1,113 @@
-// Main module of deep_sleep_tmr.ino
-// Copyright: see notice in deep_sleep_tmr.ino
+/*
+ *  See deep_sleep_tmr.ino for license and attribution.
+ */
 
 /**************************************************************
 OPERATION
-  Each time the board wakes up from deep sleep, it restarts by 
-  executing the setup() function. First it displays the number 
-  of restarts since the last reset by slowly blinking out the
-  count with the onboard yellow LED. Then, after a five second
-  delay, the LED is flashed quickly a few times indicating 
-  that the board will be put into deep sleep for another 
-  SLEEP_PERIOD.
-  
+  Each time the board wakes up from deep sleep, it executes the
+  setup() function. Near the end of the setup() function, the
+  board is put into deep sleep. Consequently the loop() function
+  is never executed and anything in the setup() function after 
+  when deep sleep is enabled will also not be executed.
+
+LED 
+  In correct operation, the LED does two things.
+  First, as the board wakes from deep sleep, it slowly 
+  flashes out the number of times the setup() function
+  has been started since the SoC was powered up. Then
+  the LED quickly flashes for 5 times announcing
+  that the SoC is going into sleep mode.
+
 NOTE
-  Define the USE_SERIAL macro to print statements to the 
-  console with the Serial peripheral. This is best done in the 
-  Arduino IDE because its serial monitor will automatically
-  reconnect with the ESP32-C6 when it wakes from deep sleep
-  and reopens its serial port. With the PlaformIO terminal
-  it is necessary to reconnect manually whenever the board
-  comes out of deep sleep.
+  The sketch can report the state of the machine to the 
+  serial monitor. However, the serial peripheral is shut
+  down when the SoC goes into sleep mode. That means 
+  that the serial port has to be reopened when comming
+  out of sleep mode. The Arduino IDE automatically 
+  reconnects to the serial port quickly enough that 
+  the messages from the sketch will be seen. In 
+  platformIO it is necessary to manually reconnect 
+  to the SoC built-in USB of the SoC so that it may 
+  not be possible to see the messages. If the board
+  uses a USB-serial adapter then this may not be 
+  a factor.
 *************************************************************/
 
 #include <Arduino.h>
 
-//#define USE_SERIAL
-
-// 15 seccond sleep period in microseconds
+//////// User configuration //////
+///
+///  Send messages on waking up from deep sleep and on going into
+///  deep sleep to the serial monitor. Undefine to stop using the 
+///  serial peripheral. See NOTE above
+#define USE_SERIAL
+///
+///  15 seccond sleep period in microseconds
+///  If USE_SERIAL is defined, do not make the sleep period too short 
+///  as the operating system may assign another serial device to the 
+///  waken up board in which case even the Arduino IDE will not reconnect.
 #define SLEEP_PERIOD 15000000
+///
+///  Settle time in milliseconds after esp_deep_sleep_enable_gpio_wakeup()
+#define SETTLE_TIME 250
+///
+///  By default LED_PIN is set to BUILTIN_LED when the latter is defined 
+///  For XIAO ESP32C3, this must be defined as it does not have a builtin LED
+///#define LED_PIN D0
+///
+///  Define the signal needed to turn on the LED
+#define LED_ON LOW 
+///
+///  Rate of USB to Serial chip if used on the development board.
+///  This is ignored when the native USB peripheral of the 
+///  ESP SoC is used.
+#define SERIAL_BAUD 115200
+///
+///  Time in milliseconds to wait after Serial.begin() in 
+///  the setup() function. If not defined, it will be set
+///  to 5000 if running in the PlaformIO IDE to manually switch
+///  to the serial monitor otherwise to 2000 if an native USB 
+///  peripheral is used or 1000 if a USB-serial adpater is used.
+///#define SERIAL_BEGIN_DELAY 8000
+///
+//////////////////////////////////
 
-// On board yellow LED
-const int ledPin = BUILTIN_LED;
-const int ledOn = LOW;
+#if !defined(ESP32)
+  #error An ESP32 based board is required
+#endif  
+
+#if (ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3, 3, 4))    
+  #error ESP32 Arduino core version 3.3.4 or newer needed
+#endif 
+
+#if !defined(SERIAL_BEGIN_DELAY)
+  #if defined(PLATFORMIO)
+    #define SERIAL_BEGIN_DELAY 5000    // 5 seconds
+  #elif (ARDUINO_USB_CDC_ON_BOOT > 0)
+    #define SERIAL_BEGIN_DELAY 2000    // 2 seconds
+  #else
+    #define SERIAL_BEGIN_DELAY 1000    // 1 second
+  #endif
+#endif 
+
+//---- Identify the ESP32 board
+
+#ifdef ARDUINO_BOARD
+  #define TITLE ARDUINO_BOARD
+#else
+  #define TITLE "Unknown ESP32 board"
+#endif        
+
+#if !defined(LED_PIN) && defined(BUILTIN_LED)
+#define LED_PIN BUILTIN_LED
+#endif
+
+const int ledPin = LED_PIN;
+const int ledOn = LED_ON;
 
 // Counter for restarts stored in non-volatile memory
 RTC_DATA_ATTR int bootCount = 0; 
+
 
 // Flash LED for count times with ms on and off times
 void blink(int count=1, int ms=50) {
@@ -45,88 +119,88 @@ void blink(int count=1, int ms=50) {
   }
 }  
 
-#if defined(USE_SERIAL)
-  // Print the sleep wakeup cause 
-  void print_wakeup_reason(void) {
-    esp_sleep_wakeup_cause_t wakeup_reason;
-    wakeup_reason = esp_sleep_get_wakeup_cause();
-    switch(wakeup_reason) {
-      case ESP_SLEEP_WAKEUP_UNDEFINED : Serial.println("Reset was not caused by exit from deep sleep"); break;
-      case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-      case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-      case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-      case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-      case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-      case ESP_SLEEP_WAKEUP_GPIO : Serial.println("Wakeup caused by GPIO (light sleep only on ESP32, S2 and S3)"); break;
-      case ESP_SLEEP_WAKEUP_UART : Serial.println("Wakeup caused by UART (light sleep only)"); break;
-      case ESP_SLEEP_WAKEUP_WIFI : Serial.println("Wakeup caused by WIFI (light sleep only)"); break;
-      case ESP_SLEEP_WAKEUP_COCPU : Serial.println("Wakeup caused by COCPU int"); break;
-      case ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG : Serial.println("Wakeup caused by COCPU crash"); break;
-      case ESP_SLEEP_WAKEUP_BT : Serial.println("Wakeup caused by BT (light sleep only)"); break;
-      default : Serial.printf("Unknown wakeup or reset reason (%d)\n",wakeup_reason); break;
-    }
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    case ESP_SLEEP_WAKEUP_GPIO : Serial.println("Wakeup caused by GPIO"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
   }
-#endif
+}
 
-void setup() { 
 
-  #if defined(USE_SERIAL)
+void setup() {
+  #if (ARDUINO_USB_CDC_ON_BOOT > 0)
   Serial.begin();
-  delay(3000);
-  Serial.println("\nDeep Sleep I/O Example");
+  delay(SERIAL_BEGIN_DELAY);
+  #else 
+  Serial.begin(SERIAL_BAUD);
+  delay(SERIAL_BEGIN_DELAY);
+  Serial.println();
+  #endif  
+
+  if (!bootCount) {
+    delay(SERIAL_BEGIN_DELAY);
+    Serial.println("\n\nProject: deep_sleep_tmr");
+    Serial.println("Purpose: Test waking the SoC from deep sleep with a timer");
+    Serial.printf("  Board: %s\n\n", TITLE);
+  }
+
+  // Increment boot count
+  bootCount++;
+
+  #ifdef USE_SERIAL
+  Serial.println("\n\nBoot number: " + String(bootCount));
   print_wakeup_reason();
   #endif
 
   // Set builtin led pin as an output
   pinMode(ledPin, OUTPUT); 
 
-  // Increment boot count
-  bootCount++;
-
-  #if defined(USE_SERIAL)
-  Serial.printf("Boot count: %d\n", bootCount);
-  #endif
-
   // Display bootcount with slow LED blinks
   blink(bootCount, 750); 
-  
-  // Delay for 5 seconds
-  delay(5000); 
 
   if (esp_sleep_enable_timer_wakeup(SLEEP_PERIOD) == ESP_OK) {
- 
-    #if defined(USE_SERIAL)   
-    Serial.println("Closing Serial peripheral and going into deep sleep after LED flashes.");
-    Serial.printf("Deep sleep will terminate in %.1f seconds\n", float(SLEEP_PERIOD/1000000));
-    Serial.flush();
-    Serial.end();
+    delay(SETTLE_TIME);
+    #ifdef USE_SERIAL
+    Serial.printf("Going into sleep mode for %.1f seconds\n", SLEEP_PERIOD/(1000*1000.0));
     #endif
-
-
     // Quick LED blinks announcing start of deep sleep
     blink(5);
-
-    // Enter deep sleep
     esp_deep_sleep_start();
-
-  } else {  
- 
-    #if not defined(USE_SERIAL)
-    Serial.begin();
-    #endif
-    while (true) {
-      delay(2000);
-      Serial.println("Cannot go into deep sleep mode");
+  } else {
+    // delay 10 seconds to give time for manual reconnection
+    delay(10000);
+    Serial.println("Could not enable timer wakeup");
+    Serial.println("Will reboot in 10 seconds");
+    unsigned long errortimer = millis();
+    while (millis() - errortimer < 10000) {
+      blink(1);
+      delay(100);
     }  
- 
-  }
+    ESP.restart();
+  }  
+
+  Serial.println("This message in setup() should never be printed");
+  while (true) {
+    blink(5);
+    delay(250);
+  }  
+
 }
 
 void loop() {
+  Serial.println("This is the loop talking. Should never happen.");
   // This odd flashing pattern should never be seen because the 
   // device is put in deep sleep in setup() before loop() starts.
   blink(4, 50);
   delay(125);
   blink(2, 100);
-  delay(2000);
+  delay(5000);
 }
