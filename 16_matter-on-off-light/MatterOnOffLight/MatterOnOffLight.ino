25b0d461ca248c0fda1a9abc6c7d48c917df6e61
sigmdel <michel@sigmdel.ca>
2026-02-17 16:53:19 -0400

    Added mention of the partition scheme

--- 16_matter-on-off-light/MatterOnOffLight/MatterOnOffLight.ino
+++ 16_matter-on-off-light/MatterOnOffLight/MatterOnOffLight.ino
@@ -1,3 +1,25 @@
+/*
+ * MatterOnOffLight.ino
+ * Demonstrates how to create a Matter-compatible on/off light device using an ESP32-C6 SoC microcontroller
+ *
+ * This is a stub to satisfy the Arduino IDE. The source code is 
+ * in the file main.cpp in the same directory.
+ *
+ * This sketch will compile in the Arduino IDE
+ *
+ *  1- Add https://espressif.github.io/arduino-esp32/package_esp32_index.json
+ *     in the Additional Boards Manager URLS in the Preferences window.
+ *  2- Install platform esp32 by Espressif version 3.3.6 or newer with the Boards Manager
+ *  3- Select the XIAO ESP32C6 board
+ *  4. Set the Partition Scheme  to "Huge APP (3MB No OTA/1MB SPIFFS)" in the Tools menu.
+ *
+ * Michel Deslierres (https://github.com/sigmdel)
+ * February 21, 2026
+ *
+ */
+
+// From the original source: 
+//
 // Copyright 2025 Espressif Systems (Shanghai) PTE LTD
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,14 +34,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- *
- * Slightlty modified to support the XIAO ESP32C6 and perhaps other 
- * ESP32-C6 or ESP32-H2 based boards
- *   - support for simple LED added
+/*
+ * Slightlty modified to support the XIAO ESP32C6 and perhaps other ESP32-C6 based boards
  *   - support for active LOW LED added
  *   - support for the XIAO ESP32C6 external antenna added
  *
- * Michel Deslierres (https://github.com/sigmdel)
- * Febrary 16, 2026
  */
